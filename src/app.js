835859c8f06ad1c2bf441657615be98a03ab0369
saha-7op <saha7pritam@gmail.com>
2025-07-17 12:00:19 +0530

    app.use() checks routes inside the code from top to bottom.

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ const express = require('express')
 
 const app=express()
 
-app.use("/",(req,res)=>{
-    res.send("Namaste")
-})
+// app.use() checks routes inside the code from top to bottom. As soon as first match comes the callback hits.
+
 
 app.use("/test",(req,res)=>{
     res.send("Testing page")
 })
 
+app.use("/hello/2",(req,res)=>{
+    res.send("hello 2 page")
+})
+
+app.use("/hello",(req,res)=>{
+    res.send("hello page")
+})
+
+app.use("/",(req,res)=>{
+    res.send("Namaste")
+})
+
+
 app.listen(3000, ()=>{
-    console.log("server is running on port 7777")
+    console.log("server is running on port 3000")
 })
