72dc445c5f79c06cfab7e682c93ed22437da732a
saha-7op <saha7pritam@gmail.com>
2025-07-18 20:45:00 +0530

    Error handlers

--- src/app.js
+++ src/app.js
@@ -4,25 +4,25 @@ const app=express()
 
 // app.use() checks routes inside the code from top to bottom. As soon as first match comes the callback hits.
 
-// app.get("/test",(req,res)=>{
-//     res.send({"name":"Pritam Saha", "age":25})
-// })
 
-app.use("/",(req,res,next)=>{
-   // res.send({"name":"Pritam Saha", "age":25})
-   console.log("1st route handler")
-   res.send("Hello")
-   next()
-   
-},
-(req,res)=>{
-    console.log("2nd route handler")
-    res.send("Hello 2")
+
+app.use("/",(err,req,res,next)=>{
+   if(err){
+    res.status(500).send("Something went wrong")
+   }
 })
 
-// app.delete("/test",(req,res)=>{
-//     res.send("Data deleted successfully")
-// })
+app.get("/test",(req,res)=>{
+   // res.send({"name":"Pritam Saha", "age":25})
+   throw new Error("efrtg")
+})
+
+app.use("/",(err,req,res,next)=>{
+   if(err){
+    res.status(500).send("Something went wrong")
+   }
+})
+
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000")
