f0d567bcf645678518a54275915d0c626e39f17a
saha-7op <saha7pritam@gmail.com>
2025-07-20 16:49:30 +0530

    Schema Model add data to database inside documents

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const express = require('express')
-
 const connectDB=require('./config/database')
+const User = require('./models/user')
 const app=express()
 
 // app.use() checks routes inside the code from top to bottom. As soon as first match comes the callback hits.
+
+app.post("/signup",async (req, res) => {
+   const user = new User({
+       firstName: "pritam",
+       lastName: "Saha",
+       email: "saha7pritam@gmail.com",
+       password: "xyz123",
+       gender: "Male",
+       age: 25
+   })
+
+   try {
+       const savedUser = await user.save()
+       res.send("User created successfully")
+   } catch (error) {
+       res.status(400).send("Error creating user: " + error.message)
+   }
+})   
+
 
 connectDB().then(()=>{
     console.log("Connected to database successfully")
