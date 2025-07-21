b405045d01f7b52555662e6a432bd28267eeefda
saha-7op <saha7pritam@gmail.com>
2025-07-21 20:48:24 +0530

    Data sanitization + Schema Validation

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ app.patch("/user", async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
   try{
-    await User.findByIdAndUpdate({_id: userId}, data)
+    await User.findByIdAndUpdate({_id: userId}, data,{runValidators: true});
     res.send("User updated successfully");
   }catch(error) {
     res.status(500).send("Error updating user: " + error.message);
