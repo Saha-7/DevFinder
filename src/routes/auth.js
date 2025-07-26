c2dfb0fb8c6a998c950a5424944e1bedd4cc4252
saha-7op <saha7pritam@gmail.com>
2025-07-26 11:39:12 +0530

    CORS options added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -51,7 +51,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
       })
-
+      
 
       res.send(user);
     }else{
