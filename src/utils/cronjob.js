15a46567b819cdcbec9fd41fbe9772db7b242a9c
saha-7op <saha7pritam@gmail.com>
2025-08-08 19:54:50 +0530

    Yesterdays requests

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -1,10 +1,24 @@
 const cron = require("node-cron")
 
-const {subDays} = require("date-fns")
+const {subDays, startOfDay, endOfDay} = require("date-fns")
+const ConnectionRequestModel = require("../models/connectionRequest")
 
 cron.schedule("0 9 * * *", ()=>{
     // Send mail to those people who got Request from Yesterday
     try{
+
+        const yesterday = subDays(new Date(),1)
+
+        const yesterdayStart = startOfDay(yesterday)
+        const yesterdayEnd = endOfDay(yesterday)
+
+        const pendingRequests = ConnectionRequestModel.find({
+            status: "interested",
+            createdAt:{
+                $gte: yesterdayStart,
+                $lt: yesterdayEnd
+            }
+        })
 
     }catch(err){
         console.log(err)
