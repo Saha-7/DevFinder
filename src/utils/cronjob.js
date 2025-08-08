3dbcb4301da375cf33b45d52648cfdb6b4112592
saha-7op <saha7pritam@gmail.com>
2025-08-08 20:48:01 +0530

    email sent from cron job

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -2,8 +2,9 @@ const cron = require("node-cron")
 
 const {subDays, startOfDay, endOfDay} = require("date-fns")
 const ConnectionRequestModel = require("../models/connectionRequest")
+const sendEmail = require("./sendEmail")
 
-cron.schedule("0 9 * * *", ()=>{
+cron.schedule("45 20 * * *", async()=>{
     // Send mail to those people who got Request from Yesterday
     try{
         const yesterday = subDays(new Date(),1)
@@ -11,7 +12,7 @@ cron.schedule("0 9 * * *", ()=>{
         const yesterdayStart = startOfDay(yesterday)
         const yesterdayEnd = endOfDay(yesterday)
 
-        const pendingRequests = ConnectionRequestModel.find({
+        const pendingRequests = await ConnectionRequestModel.find({
             status: "interested",
             createdAt:{
                 $gte: yesterdayStart,
@@ -21,8 +22,18 @@ cron.schedule("0 9 * * *", ()=>{
 
         const listOfEmails = [...new Set(pendingRequests.map(req=> req.toUserId.email))]
 
+        console.log(listOfEmails);
+        
+
         for(const email of listOfEmails){
             // Send emails
+            try{
+                const res = await sendEmail.run("New Friend Requests pending for " + email + ", Please login to your account in order to accept/reject the request.")
+                console.log(res)
+            }
+            catch(err){
+                console.log(err)
+            }
         }
 
     }catch(err){
