c4dec213f99a38eea3f411e6f5be765f7944ae42
saha-7op <saha7pritam@gmail.com>
2025-08-08 19:49:49 +0530

    date-fns setup

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -1,6 +1,13 @@
 const cron = require("node-cron")
 
+const {subDays} = require("date-fns")
+
 cron.schedule("0 9 * * *", ()=>{
-    console.log("Hello World, " + new Date());   
+    // Send mail to those people who got Request from Yesterday
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
 })
 
