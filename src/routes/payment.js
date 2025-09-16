a1b1c0ebd5240b7b0f4ca79f3c106472b0615e74
saha-7op <saha7pritam@gmail.com>
2025-09-17 00:37:14 +0530

    Instance order created

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const { userAuth } = require('../middlewares/auth')
 const paymentRouter = express.Router()
-
+const razorpayInstance = require('../utils/razorpay')
 
 paymentRouter.post("/payment/create", userAuth, async (req,res)=>{
-
+    try{
+        razorpayInstance.orders.create({
+            "amount": 50000,  // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+            "currency": "INR",
+            "receipt": "order_rcptid_11",
+            notes: {
+              "firstName": "value3",
+              "lastName": "value2",
+              membershipType: "silver"
+            }
+        })
+    }catch(err){
+        return res.status(500).json({message: "Razorpay Internal Server Error", error: err.message})
+    }
 })
 
 
